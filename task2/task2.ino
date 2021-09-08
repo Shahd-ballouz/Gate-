cedf66f0026338c9363b217e50a3e7a1405d9764
AhmedSayed54 <asayed.as954@gmail.com>
2021-09-08 17:30:20 +0200

    finaaaaaaaaaaal

--- task2/task2.ino
+++ task2/task2.ino
@@ -10,24 +10,27 @@ pinMode(LDR2,INPUT);
 pinMode(buzzer,OUTPUT);              
 }
 void loop() {  
-
-
 int LDRStatus1 = analogRead(LDR1);
 int LDRStatus2 = analogRead(LDR2);
-
-if (LDRStatus1 <= 200 && LDRStatus2 <= 200 ) {
+if (LDRStatus1 <= 200 && count < 6) {
   count++;
   digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
   delay(1000);
   }
+if (LDRStatus2 <= 200 && count != 0) {
+  count--;
+  }
+if(count == 0 ){
+    Serial.println("There are no people in the room.");
+  }
   Serial.println(count);
   Serial.println(LDRStatus1);
   Serial.println(LDRStatus2);
   delay(1000);
 if(count==5){
-  Serial.println("Maximum people have been reached");
+  Serial.println("Maximum people have been reached.");
   tone(buzzer, 100); 
   delay(2000);
   noTone(buzzer); 
